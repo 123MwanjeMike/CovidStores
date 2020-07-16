2aa12746ed3f4a3edb074b66039ae2c53246aaca
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-16 23:41:29 +0300

    Moving to MVC day 2

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
+const logins = require('./routes/login')
 
-// app.use(express.static('public'));
-// app.use(express.static('public/SalesAgent'));
-app.use(express.static('public/StoreManager'));
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.render('index', { message: 'Welcome to covidStore!', fname: 'First name', quote: 'Your quote'});
 });
 
-app.get('/agent', (req, res) => {
-    res.sendFile(__dirname + '/public/SalesAgent/index.html')
-});
+// import routes
+app.use('/login', logins);
 
-app.get('/manager', (req, res) => {
-    res.sendFile(__dirname + '/public/StoreManager/index.html')
-});
-
-app.get('*', (req,res) => {
-    res.send('error 404 <br/>Page not found')
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html')
+// });
+// app.get('/agent', (req, res) => {
+//     res.sendFile(__dirname + '/public/SalesAgent/index.html')
+// });
+// app.get('/manager', (req, res) => {
+//     res.sendFile(__dirname + '/public/StoreManager/index.html')
+// });
+// app.get('*', (req,res) => {
+//     res.send('error 404 <br/>Page not found')
+// });
 
 // Listening for requests: the server!
 app.listen(3000, () => {
