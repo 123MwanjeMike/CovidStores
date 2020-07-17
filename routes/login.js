72c4c0bcf80724fe869c73ae9479b3d5bd64bbd1
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-17 22:07:59 +0300

    Login, and password finished

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
 
 // gets and displays login page
 router.get('/', (req,res) => {
     res.render('login')
+});
+
+router.get('/passwordRecovery', (req,res) => {
+    res.render('password')
+});
+
+router.post('/authentication', (req,res) => {
+    console.log(req.body);
+    res.send('Welcome admin');
+    //Enter here code to verify kind of admin this is
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Welcome to covidStore!'});
+    res.render('index', { message: 'This is a message to render from server.js'});
 });
 
 // import routes
@@ -21,7 +21,7 @@ app.use('/login', logins);
 //     res.sendFile(__dirname + '/public/StoreManager/index.html')
 // });
 app.get('*', (req,res) => {
-    res.send('error 404 <br/>Page not found')
+    res.render('404')
 });
 
 // Listening for requests: the server!
