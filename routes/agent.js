a6b81194817bb547e6eb6e15e16d92d24a62d81e
Mwanje Mike <mwanjemike767@gmail.com>
2020-07-20 18:24:05 +0300

    Movement to MVC complete

--- routes/agent.js
+++ routes/agent.js
@@ -7,7 +7,39 @@ router.use(express.static('public'));
 
 // gets and displays login page
 router.get('/', (req, res) => {    
-    res.render('./agent/dashboard', { pageTitle: 'Dashboard' });
+    res.render('./agent/dashboard', { pageTitle: 'Dashboard', user: 'Sales Agent' });
+});
+
+router.get('/addPurchase', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'Add Purchase', user: 'Sales Agent' });
+});
+
+router.get('/updatePurchase', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'Update Purchase', user: 'Sales Agent' });
+});
+
+router.get('/recordPayment', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'Record Payment', user: 'Sales Agent' });
+});
+
+router.get('/updatePayment', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'Update Payment', user: 'Sales Agent' });
+});
+
+router.get('/balances', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'View Balances', user: 'Sales Agent' });
+});
+
+router.get('/clients', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'View Clients', user: 'Sales Agent' });
+});
+
+router.get('/calculator', (req, res) => {    
+    res.render('./agent/dashboard', { pageTitle: 'Calculator', user: 'Sales Agent' });
+});
+
+router.get('/search', (req, res) => {
+    res.render('./agent/dashboard', { pageTitle: `${req.query.search}`, user: 'Store Manager' });
 });
 
 module.exports = router;
--- routes/manager.js
+++ routes/manager.js
@@ -7,7 +7,47 @@ router.use(express.static('public'));
 
 // gets and displays login page
 router.get('/', (req, res) => {
-    res.render('./manager/dashboard', { pageTitle: 'Dashboard' });
+    res.render('./manager/dashboard', { pageTitle: 'Dashboard', user: 'Store Manager'  });
+});
+
+router.get('/registerAgent', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'Register Agent', user: 'Store Manager'  });
+});
+
+router.get('/removeAgent', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'Remove Agent', user: 'Store Manager'  });
+});
+
+router.get('/addItems', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'Add Items', user: 'Store Manager'  });
+});
+
+router.get('/updateItems', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'Update Items', user: 'Store Manager'  });
+});
+
+router.get('/removeItems', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'Remove Items', user: 'Store Manager'  });
+});
+
+router.get('/items', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'View Items', user: 'Store Manager'  });
+});
+
+router.get('/transactions', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'View Transactions', user: 'Store Manager'  });
+});
+
+router.get('/clients', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'View Clients', user: 'Store Manager' });
+});
+
+router.get('/agents', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: 'View Agents', user: 'Store Manager'  });
+});
+
+router.get('/search', (req, res) => {
+    res.render('./manager/dashboard', { pageTitle: `${req.query.search}`, user: 'Store Manager'  });
 });
 
 module.exports = router;
