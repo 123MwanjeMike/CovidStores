c8397047c80a3c86bb741afb524c9d8db9bf14b0
Mwanje Mike <mwanjemike767@gmail.com>
2020-08-01 00:50:53 +0300

    Shared table views across Manager and Agent

--- routes/agent.js
+++ routes/agent.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.static('public'));
+router.use('/public', express.static('public'));
 
 const transaction = mongoose.model('transaction');
+const LTPP = mongoose.model('LTPP');
 
 let agentIn = (req, res, next) => {
     if (!req.session.user) {
@@ -21,7 +22,7 @@ router.get('/', agentIn, (req, res) => {
 });
 
 router.get('/addPurchase', agentIn, (req, res) => {
-    res.render('./agent/newPurchase', { pageTitle: 'New Purchase', user: req.session.user });
+    res.render('./agent/purchases/newPurchase', { pageTitle: 'New Purchase', user: req.session.user });
 });
 // Route to save the purchase data into the database
 router.post('/addPurchase', agentIn, async (req, res) => {
@@ -38,7 +39,7 @@ router.post('/addPurchase', agentIn, async (req, res) => {
 router.get('/viewPurchases', agentIn, async (req, res) => {
     try {
         let allTransactions = await transaction.find();
-        res.render('./agent/viewPurchases', {pageTitle: 'Purchases', user: req.session.user, Transaction: allTransactions});
+        res.render('./agent/purchases/viewPurchases', {pageTitle: 'Purchases', user: req.session.user, Transaction: allTransactions});
     } catch (error) {
         res.redirect('/500');
         console.log(error);
@@ -53,12 +54,24 @@ router.get('/recordPayment', agentIn, (req, res) => {
     res.render('./agent/dashboard', { pageTitle: 'Record Payment', user: req.session.user });
 });
 
-router.get('/clients', agentIn, (req, res) => {
-    res.render('./agent/dashboard', { pageTitle: 'View Clients', user: req.session.user });
+router.get('/items', agentIn, async (req, res) => {
+    try {
+        let items = await LTPP.find();
+        res.render('./agent/viewItems', {pageTitle: 'Items', user: req.session.user, LTPP: items});
+    } catch (error) {
+        res.redirect('/500');
+        console.log(error);
+    }
 });
 
-router.get('/calculator', agentIn, (req, res) => {
-    res.render('./agent/dashboard', { pageTitle: 'Calculator', user: req.session.user });
+router.get('/clients', agentIn, async (req, res) => {
+    try {
+        let clients = await transaction.find();
+        res.render('./agent/viewClients', {pageTitle: 'Clients', user: req.session.user, Client: clients});
+    } catch (error) {
+        res.redirect('/500');
+        console.log(error);
+    }
 });
 
 router.get('/search', agentIn, (req, res) => {
