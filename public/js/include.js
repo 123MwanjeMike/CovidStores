0b009657de3ab9061d8be8d382575966d0b09b39
Mwanje Mike <mwanjemike767@gmail.com>
2020-08-03 17:52:19 +0300

    Item Entry: Enforced required fields

--- public/js/include.js
+++ public/js/include.js
@@ -1,55 +1,61 @@
-// Validating user input
 var validate = () => {
+    document.getElementById("submit").disabled = true;
     // Get dataset fields that need validation
-    var Firstname = document.getElementById('firstname');
-    var Lastname = document.getElementById('lastname');
-    var Street = document.getElementById('street');
-    var City = document.getElementById('city');
-    var Biography = document.getElementById('biography');
-    var Image = document.getElementById('image');
+    var photo = document.getElementById('photo');
+    var itemName = document.getElementById('name');
+    var make = document.getElementById('make');
+    var serialNo = document.getElementById('serialNo');
+    var price = document.getElementById('price');
+    var initialPay = document.getElementById('initialPay');
+    var category = document.getElementById('category');
+    var error = document.getElementById('error');
 
-    // Function to check for any empty fields
+    // Function to check for any essential empty fields
     var empty = (check) => {
         let mine = check.value.length;
         if (mine == "") {
             check.style.border = "1px solid red";
-            alert("Please enter all fields!");
+            error.textContent = "Image, item name, make, serial number, category, and price are all needed "
+            empty(check)
+        } else if (check.value === "Select Category") {
+            check.style.border = "1px solid red";
+            error.textContent = "Please select a category"
+            empty(check)
+        } else {
+            check.style.border = "";
+            error.textContent = ""
         }
     }
-    empty(Firstname);
-    empty(Lastname);
-    empty(Street);
-    empty(City);
-    empty(Biography);
-    empty(Image);
+    empty(photo);
+    empty(itemName);
+    empty(make);
+    empty(serialNo);
+    empty(category);
+    empty(price);
+    empty(initialPay);
+    document.getElementById("submit").disabled = false;
 
-    // Function to check is only characters are entered
-    var charactersOnly = (chars) => {
-        // The only accepted trend for letters in the text field sets
-        var letters = /^[a-zA-Z]+$/;
+    // Validating item name
+    // The only accepted trend for letters in this
+    var letters = /^[a-zA-Z]+$/;
 
-        if (!chars.value.match(letters)) {
-            chars.style.border = "1px solid red";
-            alert("Enter only characters for First Name, Last Name, Street, and City ");
-        }
+    if (!itemName.value.match(letters)) {
+        chars.style.border = "1px solid red";
+        alert("Enter only characters for First Name, Last Name, Street, and City ");
     }
-    charactersOnly(firstname);
-    charactersOnly(lasstname);
-    charactersOnly(street);
-    charactersOnly(city);
 
     // Checking that only image files are entered
     var imagee = /\.(gif|jpe?g|tiff|png|webp|bmp)$/;
 
-    if (!Image.value.match(imagee)) {
-        Image.style.border = "1px solid red";
-        alert("Please enter a valid image file ");
+    if (!photo.value.match(imagee)) {
+        photo.style.border = "1px solid red";
+        // alert("Please enter a valid image file ");
     }
 }
 
-var installment = () => {
-    var purchase = window.prompt("Client Telephone number?");
-    if (purchase.length != "") {
-        window.location.href = '/agent/installment/'+purchase ;
-    }
-}
+// var installment = () => {
+//     var purchase = window.prompt("Client Telephone number?");
+//     if (purchase.length != "") {
+//         window.location.href = '/agent/installment/'+purchase ;
+//     }
+// }
