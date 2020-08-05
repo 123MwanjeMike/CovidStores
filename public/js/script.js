d6c74472f356c6ad134fe9ad61d73ae9619a99ee
Mwanje Mike <mwanjemike767@gmail.com>
2020-08-06 00:05:43 +0300

    Moved form validation into a single function

--- public/js/script.js
+++ public/js/script.js
@@ -1,87 +1,35 @@
+var fname = document.getElementById('fname');
+var lname = document.getElementById('lname');
+var address = document.getElementById('address');
+var tel = document.getElementById('tel');
+var email = document.getElementById('email');
+var NIN = document.getElementById('NIN');
+var ref = document.getElementById('ref');
+var serialNo = document.getElementById('serialNo');
+var itemName = document.getElementById('itemName');
 var price = document.getElementById('price');
+var payment = document.getElementById('payment');
+var DOP = document.getElementById('DOP');
+var balance = document.getElementById('balance');
+var nextPay = document.getElementById('nextPay');
+var nDOP = document.getElementById('nDOP');
+var receipt = document.getElementById('receipt');
+var photo = document.getElementById('photo');
+var make = document.getElementById('make');
+var category = document.getElementById('category');
+var numberInStock = document.getElementById('numberInStock');
+var payInterval = document.getElementById('payInterval');
 var initialPay = document.getElementById('initialPay');
+var errors = document.getElementById('error');
+var submit = document.getElementById("submit");
 
-var validate = () => {
-    document.getElementById("submit").disabled = true;
-    // Get dataset fields that need validation
-    var photo = document.getElementById('photo');
-    var itemName = document.getElementById('name');
-    var make = document.getElementById('make');
-    var serialNo = document.getElementById('serialNo');
-    var category = document.getElementById('category');
-    var numberInStock = document.getElementById('numberInStock');
-    var payInterval = document.getElementById('payInterval');
-    var error = document.getElementById('error');
-
-    // Function to check for any essential empty fields
-    var empty = (check) => {
-        let mine = check.value.length;
-        if (mine == "") {
-            check.style.border = "1px solid red";
-            error.textContent = "Image, item name, make, serial number, category, and price are all needed!"
-            empty(check)
-        } else if (check.value === "Select Category") {
-            check.style.border = "1px solid red";
-            error.textContent = "Please select a category!"
-            empty(check)
-        } else {
-            check.style.border = "";
-            error.textContent = ""
-        }
-    }
-    empty(photo);
-    empty(itemName);
-    empty(make);
-    empty(category);
-    empty(numberInStock);
-    empty(price);
-    empty(initialPay);
-
-    // Validating item name
-    var letters = /^[a-zA-Z\s]+$/;
-    if (!itemName.value.match(letters)) {
-        itemName.style.border = "1px solid red";
-        error.textContent = "Please enter only characters(no numbers) in Name input!";
-    }
-    // Validating item make
-    var makeregex = /^[A-Z]{2}$/;
-    if (!make.value.match(makeregex)) {
-        make.style.border = "1px solid red";
-        error.textContent = "Please enter strictly 2 capital letters in Make input!";
-    }
-
-    // Serial number
-    var alphanumeric = /^[a-zA-Z0-9_]*$/;
-    if (serialNo.value.length < 6 || serialNo.value.length > 22) {
-        serialNo.style.border = "1px solid red";
-        error.textContent = "Serial number should have 6 to 22 characters";
-    } else if (!serialNo.value.match(alphanumeric)) {
-        serialNo.style.border = "1px solid red";
-        error.textContent = "Please enter only alphanumeric characters"
-    }
-
-    // Validating payment fields
-    var numberegex = /^[0-9]+$/;
-    if (!numberInStock.value.match(numberegex)) {
-        numberInStock.style.border = "1px solid red";
-        error.textContent = "Please enter a number in Number in stock!";
-    }
-    if (!price.value.match(numberegex)) {
-        price.style.border = "1px solid red";
-        error.textContent = "Please enter a number in Price!";
-    }
-    if (!payInterval.value.match(numberegex)) {
-        payInterval.style.border = "1px solid red";
-        error.textContent = "Please enter a number in Pay interval";
-    }
-
-    // Re-enabling the submit button after validation
-    document.getElementById("submit").disabled = false;
-}
 // First installment calc
 var firstInstall = () => {
+    var price = document.getElementById('price');
+    var initialPay = document.getElementById('initialPay');
     initialPay.value = price.value - (price.value / 2);
 }
+
 //This will ensure serial number is unique and will be set to a default value if no serial number is entered
 var serialNumber = () => {
     var serialNo = document.getElementById('serialNo');
@@ -90,33 +38,64 @@ var serialNumber = () => {
     }
 }
 
-var purchase = () => {
-    var errors = document.getElementById('error');
-    function required(input, error) {
-        this.input = input.value
-        this.error = error
-        this.present = () => {
-            if (this.input == "") {
-                input.style.border = "1px solid red";
-                errors.textContent = "Please enter " + this.error + "!"
-            }else{
-                input.style.border = "";
-                errors.textContent = "";
-            }
+var required = (input, error, regex) => {
+    if (input.value.length == "") {
+        input.style.border = "1px solid red";
+        errors.textContent = "Please enter " + error + "!"
+        submit.disabled = true;
+        return false;
+    } else {
+        input.style.border = "";
+        errors.textContent = "";
+        submit.disabled = false;
+    }
+    if (regex != undefined) {
+        if (!input.value.match(regex)) {
+            input.style.border = "1px solid red";
+            errors.textContent = "Please enter a valid " + error + "!"
+            submit.disabled = true;
+            return false;
+        } else {
+            input.style.border = "";
+            errors.textContent = "";
+            submit.disabled = false;
         }
     }
-    // function regex(input, error, regex) {
-    //     required.call(input, error)
-    //     this.regex = regex
-    //     this.format = () => {
-    //         if (!this.input.value.match(this.regex)) {
-    //             input.style.border = "1px solid red";
-    //             errors.textContent = "Please enter a valid" +this.error+ "!"
-    //         }
-    //     }
-    // }
-    // regex.prototype = new required();
-    var itemname = new required(document.getElementById('itemName'), "item name")
-    itemname.present()
-    // itemname.format()
+}
+
+var validate = () => {
+    if (required(make, "make", '^[A-Z]{2}$') === false) return 0
+    if (required(photo, "item image") === false) return 0
+    if (required(itemName, "item name", '^[a-zA-Z\s]+$') === false) return 0
+    if (required(category, "a select category") == 0) return 0
+    if (required(serialNo, "a serial number", '^[a-zA-Z0-9_]*$') == 0) return 0
+    if (required(numberInStock, "number of items in stock", '^[0-9]+$') == 0) return 0
+    if (required(price, "price", '^[0-9]+$') == 0) return 0
+    if (required(initialPay, "initial pay", '^[0-9]+$') == 0) return 0
+    if (required(payInterval, "pay interval", '^[0-9]+$') == 0) return 0
+
+    // Serial number
+    if (serialNo.value.length < 6 || serialNo.value.length > 22) {
+        serialNo.style.border = "1px solid red";
+        error.textContent = "Serial number should have 6 to 22 characters";
+    }
+}
+
+
+var purchase = () => {
+    if (required(fname, "first name") == 0) return 0
+    if (required(lname, "last name") == 0) return 0
+    if (required(address, "address") == 0) return 0
+    if (required(tel, "telephone number") == 0) return 0
+    if (required(email, "email") == 0) return 0
+    if (required(NIN, "National ID number") == 0) return 0
+    if (required(ref, "referee number") == 0) return 0
+    if (required(serialNo, "serial number") == 0) return 0
+    if (required(itemName, "item name") == 0) return 0
+    if (required(price, "price") == 0) return 0
+    if (required(payment, "payment") == 0) return 0
+    if (required(DOP, "date of payment") == 0) return 0
+    if (required(balance, "balance") == 0) return 0
+    if (required(nextPay, "date of next pay") == 0) return 0
+    if (required(receipt, "receipt number") == 0) return 0
 }
