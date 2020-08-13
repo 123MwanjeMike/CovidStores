12e244bc7e003af8ec76fed0c0fc2cb5dabe3ac9
Mwanje Mike <mwanjemike767@gmail.com>
2020-08-13 17:33:11 +0300

    make it better

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('*', (req, res) => {
 });
 
 // Listening for requests: the server!
-const port = 3000;
+const port = (process.env.PORT || 3000);
 app.listen(port, (error) => {
     console.log(`Listening on port ${port}`)
 });
